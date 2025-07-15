efa8da1642d0ba0720f751c514ad230b9b79cc4b
Alastair Tse <alastair@liquidx.net>
2025-07-15 19:10:08 +0900

    Initial rendering

---
+++ src/lib/components/Index.svelte
@@ -0,0 +1,3 @@
+<div class="h-96 w-96 p-4">
+	<div class="font-bold text-amber-700">aversion</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Index from '$lib/components/Index.svelte';
+</script>
+
+<div class="p-4">
+	<Index />
+</div>
