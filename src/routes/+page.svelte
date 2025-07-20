32aa98de0938f991d29de5783e94b85fa02f18b3
Alastair Tse <alastair@liquidx.net>
2025-07-20 20:37:02 +0900

    Add codepoet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,8 @@
 
 <ul class="p-4">
 	<li class="mx-4 list-disc px-1"><a class="hover:underline" href="/of/terminal">terminal</a></li>
-	<li class="mx-4 list-disc px-1"><a class="hover:underline" href="/of/number">number</a></li>
+	<li class="mx-4 list-disc px-1">
+		<a class="hover:underline" href="/of/number">threetextfield</a>
+	</li>
+	<li class="mx-4 list-disc px-1"><a class="hover:underline" href="/of/codepoet">codepoet</a></li>
 </ul>
