cd86a312376cd31b9517692f15e9596e05f1811e
Alastair Tse <alastair@liquidx.net>
2026-03-04 16:48:36 +0900

    Add bit of stub for partykit.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="p-4">
